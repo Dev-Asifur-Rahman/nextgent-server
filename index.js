ef5072b0d13b16a6aca35732086bc0fe879a751e
Asifur Rahman <asifurrahman.ac@gmail.com>
2025-10-25 20:19:04 +0600

    server making completed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const url = require("url");
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const http = require("http");
+const { ObjectId } = require("mongodb");
 const port = 5000;
 const uri = `mongodb+srv://${process.env.DB_NAME}-${process.env.DB_PASS}@cluster0.mcintht.mongodb.net/?appName=Cluster0`;
 
@@ -22,7 +23,6 @@ async function run() {
     );
     const server = http.createServer(async (req, res) => {
       const parsedUrl = url.parse(req.url, true);
-      console.log(parsedUrl);
       if (req.url === "/") {
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("Home Server");
@@ -33,13 +33,21 @@ async function run() {
 
         const filter = {};
 
-        if (type) filter.type = '';
-        if (category) filter.category = category;
+        if (type) filter.type = type;
+        if (category) filter.category = '';
         if (location) filter.location = location;
 
         const all_properties = await property_collection.find(filter).toArray();
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify(all_properties));
+      } else if (parsedUrl.pathname === "/projects") {
+        const projectId = parsedUrl.query.id;
+        const object = await property_collection.findOne({
+          _id: new ObjectId(projectId),
+        });
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(object));
       }
     });
 
