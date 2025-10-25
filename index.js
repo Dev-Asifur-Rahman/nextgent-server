7b62f906f07d1f6a26832b84f103dca88aab8a53
Asifur Rahman <asifurrahman.ac@gmail.com>
2025-10-25 18:48:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const http = require("http");
+const port = 5000;
+const uri = `mongodb+srv://${process.env.DB_NAME}-${process.env.DB_PASS}@cluster0.mcintht.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const property_database = client.db("properties");
+    const property_collection = property_database.collection(
+      "property_collection"
+    );
+    const server = http.createServer(async (req, res) => {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+    });
+    
+    server.listen(port, () => {
+      console.log(`server is running on ${port}`);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
